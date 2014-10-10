c83db5e400e761a41203e9b3b9382e5f7db361ad
Hayden Blauzvern <hblauzvern@gmail.com>
2014-10-10 16:57:26 -0700

    Initial Commit

---
+++ Security/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Security
+//
+//  Created by Hayden Blauzvern on 10/10/14.
+//  Copyright (c) 2014 Blauzvern Gilkinson Kutsko Valentine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Security/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Security
+//
+//  Created by Hayden Blauzvern on 10/10/14.
+//  Copyright (c) 2014 Blauzvern Gilkinson Kutsko Valentine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
