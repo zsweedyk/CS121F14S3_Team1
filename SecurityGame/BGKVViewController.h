739a96c573fa245f0af972eaa6b9f07b9e1ebcab
Hayden Blauzvern <hblauzvern@gmail.com>
2014-10-22 21:41:37 -0700

    Added the ability to play music. Need to use singleton class.

--- SecurityGame/BGKVViewController.h
+++ SecurityGame/BGKVViewController.h
@@ -7,8 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+@import AVFoundation;
 
 @interface BGKVViewController : UIViewController
+
+@property(nonatomic, strong) AVAudioPlayer *backgroundMusic;
 
 - (IBAction) goToMainMenu:(UIStoryboardSegue *)segue;
 
--- SecurityGame/BGKVViewController.m
+++ SecurityGame/BGKVViewController.m
@@ -17,7 +17,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // FIX ME: How to handle not reinitializing the same view multiple times.
+    // FIX ME: All of the views always persist. That doesn't seem right. Need to
+    //         pop and push views on and off properly, or preload them.
+
+    // From StackOverflow on how to play music
+//    NSURL *musicFile = [[NSBundle mainBundle] URLForResource:@"spy_music"
+//                                               withExtension:@"mp3"];
+//    self.backgroundMusic = [[AVAudioPlayer alloc] initWithContentsOfURL:musicFile
+//                                                                  error:nil];
+//    self.backgroundMusic.numberOfLoops = -1;
+//    [self.backgroundMusic play];
 }
 
 - (void)didReceiveMemoryWarning
