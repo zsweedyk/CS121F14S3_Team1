878e28dd7a5f25077768baa3fbe898a0ce0c2bd6
Hayden Blauzvern <hblauzvern@gmail.com>
2014-11-10 19:36:08 -0800

    Created spinning wheel to force reading. Need to add RBLink, button that segues to RBLink

---
+++ SecurityGame/BGKVLoadScreenViewController.h
@@ -0,0 +1,15 @@
+//
+//  BGKVLoadScreenViewController.h
+//  SecurityGame
+//
+//  Created by Hayden Blauzvern on 11/10/14.
+//  Copyright (c) 2014 BGKV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGKVLoadScreenViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+
+@end
