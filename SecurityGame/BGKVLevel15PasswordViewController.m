424ba0c763b0c55de56a4a48aeb671128e73453f
Hayden Blauzvern <hblauzvern@gmail.com>
2014-11-29 01:37:50 -0800

    Added comments, fixed font changing issue

--- SecurityGame/BGKVLevel15PasswordViewController.m
+++ SecurityGame/BGKVLevel15PasswordViewController.m
@@ -16,31 +16,41 @@
 
 - (IBAction)trackSelections:(UIButton *)sender
 {
-    // FIX ME: Why is this complaining?
-    if ([_correctAnswers objectAtIndex:_currentEmailLevel] == sender.tag) {
+    // Check if the tag of "Send" or "Don't send" matches the expected answer
+    if ([[_correctAnswers objectAtIndex:_currentEmailLevel] integerValue] == sender.tag) {
         _currentEmailLevel += 1;
         if (_currentEmailLevel == _correctAnswers.count) {
             self.continueButton.hidden = NO;
             
             self.sendButton.hidden = YES;
             self.dontSendButton.hidden = YES;
+            
+            self.informationField.selectable = YES;
             self.informationField.text = @"Good work! Continue on!";
+            self.informationField.selectable = NO;
         }
         [self updateInformationField];
     } else {
-        [self displayWrongAnswerInformation:sender.tag];
+        self.sendButton.hidden = YES;
+        self.dontSendButton.hidden = YES;
+        [self displayWrongAnswerInformation:_currentEmailLevel];
         self.tryAgainButton.hidden = NO;
     }
 }
 
+// Updates text field to contain question
 - (void)updateInformationField
 {
+    // http://stackoverflow.com/questions/19049917/uitextview-font-is-being-reset-after-settext
+    // For some silly reason, changing the content of a textfield always changes the font size and color
+    // Making it selectable fixes it. We then disable selectable after the update.
+    self.informationField.selectable = YES;
     switch (_currentEmailLevel) {
         case 0:
             self.informationField.text = @"Hello there, I see you just got this email! Please click \"Send Info\" to start up this service.";
             break;
         case 1:
-            self.informationField.text = @"Hello there, I don't know you and want to be your friend through email! Can you send me a good email to reach you at?";
+            self.informationField.text = @"Hello there, I don't know you or your friends and would like to friend them all! Can you send me all of their email addresses?";
             break;
         case 2:
             self.informationField.text = @"Hello there, I would like to send you A LOT of money but I don't have your address? Can you send me your address and phone number?";
@@ -53,22 +63,34 @@
             break;
             
     }
+    self.informationField.selectable = NO;
 }
 
+// Reset email level and update fields
 - (IBAction)tryAgain:(id)sender
 {
     _currentEmailLevel = 0;
     [self updateInformationField];
     self.tryAgainButton.hidden = YES;
+    self.sendButton.hidden = NO;
+    self.dontSendButton.hidden = NO;
 }
 
 - (void)displayWrongAnswerInformation:(NSUInteger)i
 {
+    self.informationField.selectable = YES;
     if (i == 0) {
-        self.informationField.text = @"This was actually a bad thing to send! You don't want to disclose that kind of information! Try again!";
-    } else {
-        self.informationField.text = @"This was actually fine to send! You're not giving away any secret information! Try again!";
+        self.informationField.text = @"Make sure you follow the instructions!";
+    } else if (i == 1) {
+        self.informationField.text = @"You don't want to give out emails to strangers! Try again!";
+    } else if (i == 2) {
+        self.informationField.text = @"You shouldn't give out personal information like addresses and phone numbers to unknown people online! Try again!";
+    } else if (i == 3) {
+        self.informationField.text = @"Turn in your homework on time, and if your teacher asks for it, turn it in! Try again!";
+    } else if (i == 4) {
+        self.informationField.text = @"Never, ever, ever give out your password, to anyone! No email should ever ask you for your password. Try again!";
     }
+    self.informationField.selectable = NO;
 }
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
@@ -89,6 +111,8 @@
 {
     [super viewDidLoad];
     _currentEmailLevel = 0;
+    
+    // Boolean array containing whether the user should tap "Send" or "Don't send"
     _correctAnswers = @[@0,@1,@1,@0,@1];
 }
 
