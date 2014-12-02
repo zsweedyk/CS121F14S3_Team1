26e0ecd4ac2af47879a2232a2a6a79a9f6f368c6
Guest User <Guest@poinsettia.cs.hmc.edu>
2014-12-01 21:23:45 -0800

    Added the branch for cutscene implementation, and started (added files).

---
+++ SecurityGame/BGKVCutsceneViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGKVCutsceneViewController.h
+//  SecurityGame
+//
+//  Created by Josh Kutsko on 12/1/14.
+//  Copyright (c) 2014 BGKV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGKVCutsceneViewController : UIViewController
+
+@end
---
+++ SecurityGame/BGKVCutsceneViewController.m
@@ -0,0 +1,38 @@
+//
+//  BGKVCutsceneViewController.m
+//  SecurityGame
+//
+//  Created by Josh Kutsko on 12/1/14.
+//  Copyright (c) 2014 BGKV. All rights reserved.
+//
+
+#import "BGKVCutsceneViewController.h"
+
+@interface BGKVCutsceneViewController ()
+
+@end
+
+@implementation BGKVCutsceneViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
