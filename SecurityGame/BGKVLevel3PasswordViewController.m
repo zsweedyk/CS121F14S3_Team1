c638fa002d03df199669919adb91f1065036a7d2
Sarah Gilkinson <sgilkinson@hmc.edu>
2014-12-02 18:58:06 -0800

    Adding new images for Level3

--- SecurityGame/BGKVLevel3PasswordViewController.m
+++ SecurityGame/BGKVLevel3PasswordViewController.m
@@ -14,6 +14,20 @@
 
 @implementation BGKVLevel3PasswordViewController
 
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.passwordField.delegate = self;
+}
+
+// Level password checker
 - (BOOL)checkPassword:(NSString *)guess
 {
     NSString* password = @"Taylor";
@@ -45,41 +59,15 @@
     return NO;
 }
 
+// Segue from "Okay" button to loading screen
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
     [self performSegueWithIdentifier:@"SegueToLoadScreen" sender:self];
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    self.passwordField.delegate = self;
-}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
- {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 
 @end
